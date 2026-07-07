307faf073f1c2412cc0305265765fcde52742133
NStec-26002 <nstec26_002@fullness.email>
2026-07-07 15:02:10 +0900

    PriceCaculaterクラスのテストの内容修正

--- app/src/test/java/java_exercise/service/PriceCalculatorTest.java
+++ app/src/test/java/java_exercise/service/PriceCalculatorTest.java
@@ -6,9 +6,10 @@ import org.junit.jupiter.api.Test;
 
 public class PriceCalculatorTest {
 
+    PriceCalculator priceCalculator = new PriceCalculator();
+
     @Test
     public void testCalcPrice_case1() {
-        PriceCalculator priceCalculator = new PriceCalculator();
         long actual = priceCalculator.calcPrice(1000, 1, 1, 1);
         int expected = 950;
         assertEquals(expected, actual);
@@ -16,7 +17,6 @@ public class PriceCalculatorTest {
 
     @Test
     public void testCalcPrice_case2() {
-        PriceCalculator priceCalculator = new PriceCalculator();
         long actual = priceCalculator.calcPrice(1000, 1, 2, 1);
         int expected = 950;
         assertEquals(expected, actual);
@@ -24,7 +24,6 @@ public class PriceCalculatorTest {
 
     @Test
     public void testCalcPrice_case3() {
-        PriceCalculator priceCalculator = new PriceCalculator();
         long actual = priceCalculator.calcPrice(1000, 1, 1, 3);
         int expected = 800;
         assertEquals(expected, actual);
@@ -32,7 +31,6 @@ public class PriceCalculatorTest {
 
     @Test
     public void testCalcPrice_case4() {
-        PriceCalculator priceCalculator = new PriceCalculator();
         long actual = priceCalculator.calcPrice(1000, 1, 3, 2);
         int expected = 900;
         assertEquals(expected, actual);
@@ -40,7 +38,6 @@ public class PriceCalculatorTest {
 
     @Test
     public void testCalcPrice_case5() {
-        PriceCalculator priceCalculator = new PriceCalculator();
         long actual = priceCalculator.calcPrice(1000, 1, 3, 1);
         int expected = 1000;
         assertEquals(expected, actual);
@@ -48,7 +45,6 @@ public class PriceCalculatorTest {
 
     @Test
     public void testCalcPrice_case6() {
-        PriceCalculator priceCalculator = new PriceCalculator();
         long actual = priceCalculator.calcPrice(1000, 1, 1, 2);
         int expected = 1000;
         assertEquals(expected, actual);
@@ -56,7 +52,6 @@ public class PriceCalculatorTest {
 
     @Test
     public void testCalcPrice_case7() {
-        PriceCalculator priceCalculator = new PriceCalculator();
         long actual = priceCalculator.calcPrice(1000, 2, 1, 3);
         int expected = 1000;
         assertEquals(expected, actual);
@@ -64,7 +59,6 @@ public class PriceCalculatorTest {
 
     @Test
     public void testCalcPrice_case8() {
-        PriceCalculator priceCalculator = new PriceCalculator();
         long actual = priceCalculator.calcPrice(1000, 1, 1, 4);
         int expected = 1000;
         assertEquals(expected, actual);
